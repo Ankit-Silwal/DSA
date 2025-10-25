a5ea2b780f042ba774ed9b27ccef7d016994daa4
GubbinLubbin <silwalankit27@gmail.com>
2025-10-26 04:35:44 +0545

    Some recursione done still left lect no 29

---
+++ src/Recursion/RecursionPatterns/RecursionTriangle.java
@@ -0,0 +1,20 @@
+package Recursion.RecursionPatterns;
+
+public class RecursionTriangle {
+  public static void main(String[] args) {
+    triangle(5, 0);
+  }
+
+  static void triangle(int row, int col) {
+    if (row == 0) {
+      return;
+    }
+    if (col < row) {
+      triangle(row, col + 1); 
+      System.out.print("*");  
+    } else {
+      triangle(row - 1, 0);   
+      System.out.println();   
+    }
+  }
+}
