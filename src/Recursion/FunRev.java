0b410805188e3962a3e44f497f3f6a757b2457f4
GubbinLubbin <silwalankit27@gmail.com>
2025-10-26 01:55:18 +0545

    Recursion sorted array

---
+++ src/Recursion/FunRev.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class FunRev {
+  public static void main(String[] Args){
+    int n=5;
+    funRev(n);
+  }
+  static void funRev(int n){
+    if(n==0){
+      return;
+    }
+    funRev(n-1);
+    System.out.print(n+" ");
+  }
+}
