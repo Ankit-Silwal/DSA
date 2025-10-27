1fc61214765cb4f4a3d32461a83fc2911bef3e12
GubbinLubbin <silwalankit27@gmail.com>
2025-10-27 14:14:00 +0545

    Origin simple

---
+++ src/Subsets/SubSeq.java
@@ -0,0 +1,15 @@
+package Subsets;
+public class SubSeq{
+  public static void main(String[] args) {
+    subseq("","abcdef");
+  }
+  static void subseq(String p,String up){
+    if(up.isEmpty()){
+      System.out.printf("[%s] ,",p);
+      return;
+    }
+    char ch=up.charAt(0); 
+    subseq(p+ch,up.substring(1));
+    subseq(p,up.substring(1));
+  }
+}
