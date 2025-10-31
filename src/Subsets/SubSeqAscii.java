6a2aee0b2cef8736b8d72a4cd45bf15564115fc8
GubbinLubbin <silwalankit27@gmail.com>
2025-10-31 07:44:19 +0530

    Subset of the arrays to remember

---
+++ src/Subsets/SubSeqAscii.java
@@ -0,0 +1,22 @@
+package Subsets;
+import java.util.ArrayList;
+public class SubSeqAscii {
+  public static void main(String[] args) {
+      String value="dfh";
+      System.out.println(subsetascii("",value));
+  }
+  static ArrayList<String> subsetascii(String p,String up){
+    if(up.length()==0){
+      ArrayList <String> ans=new ArrayList<>();
+      ans.add(p);
+      return ans;
+    }
+    char ch=up.charAt(0);
+    ArrayList<String> left=subsetascii(p,up.substring(1));
+    ArrayList<String> middle=subsetascii(p+ch,up.substring(1));
+    ArrayList<String> right=subsetascii(p+(int)ch,up.substring(1));
+    left.addAll(middle);
+    left.addAll(right);
+    return left;
+  }
+}
